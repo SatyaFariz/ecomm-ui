5526e84fbe41b5be10dd7786c3c146f6e2907e01
Satya Fariz <satyafariznur@gmail.com>
2022-09-03 01:47:12 +0700

    add icons

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,13 +1,20 @@
 import type { NextPage } from 'next'
+import { AiOutlineSearch, AiOutlineShopping } from 'react-icons/ai'
+import styles from './AppHeader.module.css'
 
 const AppHeader: NextPage = () => {
   return (
-    <div className="sticky h-[64px] top-0 flex items-center border-b border-slate-900/10">
+    <div className={styles.header}>
         <img
-            className="ml-4"
+            className={styles.logo}
             src="https://werkidz.mygostore.com/_next/static/images/goStore-logo-64f7ac0ca37c2341f8ae739a75345580.svg"
             alt="app logo"
         />
+
+        <div className={styles.icons}>
+            <AiOutlineSearch size={24}/>
+            <AiOutlineShopping size={24}/>
+        </div>
     </div>
   )
 }
