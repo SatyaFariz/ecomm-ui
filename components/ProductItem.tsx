060d8c7604ef862f030160c757030d7cef762335
Satya Fariz <satyafariznur@gmail.com>
2022-09-03 22:26:58 +0700

    add link

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,21 +1,24 @@
+import Link from 'next/link'
 import styles from './ProductItem.module.css'
 
 const ProductItem = (props: any) => {
     const { product } = props
     return (
-        <div className={styles.container}>
-            {product.sale &&
-            <div className={styles.discountPercentage}>
-                {product.sale.discount_percentage}%
+        <Link href={`/products/${product.id}`}>
+            <div className={styles.container}>
+                {product.sale &&
+                <div className={styles.discountPercentage}>
+                    {product.sale.discount_percentage}%
+                </div>
+                }
+                <img src={`http://192.168.1.101/media/catalog/product${product.media_gallery_entries[0]?.file}`}/>
+                <p className={styles.name}>{product.name}</p>
+                <p className={styles.price}>Rp {product.sale?.price || product.price}</p>
+                {product.sale &&
+                <p className={styles.salePrice}>Rp {product.price}</p>
+                }
             </div>
-            }
-            <img src={`http://192.168.1.101/media/catalog/product${product.media_gallery_entries[0]?.file}`}/>
-            <p className={styles.name}>{product.name}</p>
-            <p className={styles.price}>Rp {product.sale?.price || product.price}</p>
-            {product.sale &&
-            <p className={styles.salePrice}>Rp {product.price}</p>
-            }
-        </div>
+        </Link>
     )
   }
   
