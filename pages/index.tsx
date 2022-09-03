e2f3bea1c256f96a5d2706ddf34c783e92f00f73
Satya Fariz <satyafariznur@gmail.com>
2022-09-04 01:30:27 +0700

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,10 @@ import Layout from '../components/Layout'
 import ProductList from '../components/ProductList'
 
 const Home = () => {
-    const { isLoading, error, data }: any = useQuery('repoData', () =>
+    const { isLoading, error, data }: any = useQuery('product_list_home', () =>
         fetch('/api/products?page=1&limit=10').then(res => res.json())
     )
-    console.log(data)
+    
     if (isLoading) return 'Loading...'
  
     if (error) return 'An error has occurred: ' + error.message
--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,17 +1,21 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import { useQuery } from 'react-query'
 import Layout from '../../components/Layout'
 
 const Product = () => {
-    const { isLoading, error, data }: any = useQuery('repoData', () =>
-        fetch('/api/products?page=1&limit=10').then(res => res.json())
+    const router = useRouter()
+    const { id } = router.query
+    const { isLoading, error, data }: any = useQuery(`product_detail_${id}`, () =>
+        fetch(`/api/products/${id}`).then(res => res.json())
     )
-    console.log(data)
+    
     if (isLoading) return 'Loading...'
  
     if (error) return 'An error has occurred: ' + error.message
     return (
         <Layout>
+            <img src={`http://192.168.1.101/media/catalog/product${data.media_gallery_entries[0]?.file}`}/>
             <div className="p-5">
                 PRODUCT PAGE
             </div>
