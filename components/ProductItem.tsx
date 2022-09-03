2e9632c096ae45b7628d5b7863bbd1f8325c53d1
Satya Fariz <satyafariznur@gmail.com>
2022-09-03 22:06:44 +0700

    update opacity

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -3,7 +3,12 @@ import styles from './ProductItem.module.css'
 const ProductItem = (props: any) => {
     const { product } = props
     return (
-        <div>
+        <div className={styles.container}>
+            {product.sale &&
+            <div className={styles.discountPercentage}>
+                {product.sale.discount_percentage}%
+            </div>
+            }
             <img src={`http://192.168.1.101/media/catalog/product${product.media_gallery_entries[0]?.file}`}/>
             <p className={styles.name}>{product.name}</p>
             <p className={styles.price}>Rp {product.sale?.price || product.price}</p>
