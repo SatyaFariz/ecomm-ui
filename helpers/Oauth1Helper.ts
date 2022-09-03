00089f04c7e865ef6383de4f2de72e7efd468412
Satya Fariz <satyafariznur@gmail.com>
2022-09-03 23:24:05 +0700

    add helpers folder

---
+++ helpers/Oauth1Helper.ts
@@ -0,0 +1,30 @@
+const oauth1a = require('oauth-1.0a')
+import { createHmac } from 'crypto'
+
+class Oauth1Helper {
+    static getAuthHeaderForRequest(request: any) {
+      const CONSUMERKEY = process.env.MAGENTO_CONSUMER_KEY
+      const CONSUMERSECRET = process.env.MAGENTO_CONSUMER_SECRET
+      const TOKENKEY = process.env.MAGENTO_TOKEN_KEY
+      const TOKENSECRET = process.env.MAGENTO_TOKEN_SECRET
+    
+      const oauth = oauth1a({
+          consumer: { key: CONSUMERKEY, secret: CONSUMERSECRET },
+          signature_method: 'HMAC-SHA256',
+          hash_function(base_string: string, key: string) {
+              return createHmac('sha256', key)
+                  .update(base_string)
+                  .digest('base64')
+          },
+      })
+
+      const authorization = oauth.authorize(request, {
+          key: TOKENKEY,
+          secret: TOKENSECRET,
+      })
+
+      return oauth.toHeader(authorization);
+    }
+}
+
+export default Oauth1Helper
--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,35 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
 import qs from 'query-string'
-const oauth1a = require('oauth-1.0a')
-const crypto = require('crypto')
-
-class Oauth1Helper {
-    static getAuthHeaderForRequest(request: any) {
-      const CONSUMERKEY = process.env.MAGENTO_CONSUMER_KEY
-      const CONSUMERSECRET = process.env.MAGENTO_CONSUMER_SECRET
-      const TOKENKEY = process.env.MAGENTO_TOKEN_KEY
-      const TOKENSECRET = process.env.MAGENTO_TOKEN_SECRET
-    
-      const oauth = oauth1a({
-          consumer: { key: CONSUMERKEY, secret: CONSUMERSECRET },
-          signature_method: 'HMAC-SHA256',
-          hash_function(base_string: string, key: string) {
-              return crypto
-                  .createHmac('sha256', key)
-                  .update(base_string)
-                  .digest('base64')
-          },
-      })
-
-      const authorization = oauth.authorize(request, {
-          key: TOKENKEY,
-          secret: TOKENSECRET,
-      })
-
-      return oauth.toHeader(authorization);
-    }
-}
+import Oauth1Helper from '../../../helpers/Oauth1Helper'
 
 // type Data = {
 //   name: string
