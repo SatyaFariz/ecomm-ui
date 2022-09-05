08ece2c1c1194d65f758dcf5bd7cfa5e03addb54
Satya Fariz <satyafariznur@gmail.com>
2022-09-06 04:42:36 +0700

    can toggle between searching and not

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,31 +1,34 @@
 import type { NextPage } from 'next'
 import { AiOutlineSearch, AiOutlineShopping, AiOutlineLeft } from 'react-icons/ai'
+import { useState } from 'react'
 import styles from './AppHeader.module.css'
 
 const AppHeader: NextPage = () => {
-  return (
-    <header className={styles.header}>
-        <div className={styles.container} style={{ display: 'none' }}>
-            <img
-                src="https://werkidz.mygostore.com/_next/static/images/goStore-logo-64f7ac0ca37c2341f8ae739a75345580.svg"
-                alt="app logo"
-            />
+    const [isSearching, setIsSearching] = useState(false)
 
-            <div className={styles.icons}>
-                <AiOutlineSearch className={styles.icon}/>
-                <AiOutlineShopping className={styles.icon}/>
+    return (
+        <header className={styles.header}>
+            <div className={styles.container} style={{ display: isSearching ? 'none' : undefined }}>
+                <img
+                    src="https://werkidz.mygostore.com/_next/static/images/goStore-logo-64f7ac0ca37c2341f8ae739a75345580.svg"
+                    alt="app logo"
+                />
+
+                <div className={styles.icons}>
+                    <AiOutlineSearch className={styles.icon} onClick={() => setIsSearching(true)}/>
+                    <AiOutlineShopping className={styles.icon}/>
+                </div>
             </div>
-        </div>
 
-        <div className={styles.searchbar} style={{ display: undefined }}>
-            <AiOutlineLeft className={styles.icon}/>
+            <div className={styles.searchbar} style={{ display: isSearching ? undefined : 'none' }}>
+                <AiOutlineLeft className={styles.icon} onClick={() => setIsSearching(false)}/>
 
-            <div className={styles.searchbox}>
-                <input className={styles.textinput} placeholder="Search" type="text" spellCheck="false"/>
+                <div className={styles.searchbox}>
+                    <input className={styles.textinput} placeholder="Search" type="text" spellCheck="false"/>
+                </div>
             </div>
-        </div>
-    </header>
-  )
+        </header>
+    )
 }
 
 export default AppHeader
