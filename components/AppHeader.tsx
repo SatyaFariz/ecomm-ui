6816a7c644428895682bc6813019a5dbc067db90
Satya Fariz <satyafariznur@gmail.com>
2022-09-06 04:00:14 +0700

    add container

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -5,15 +5,17 @@ import styles from './AppHeader.module.css'
 const AppHeader: NextPage = () => {
   return (
     <header className={styles.header}>
-        <img
-            className={styles.logo}
-            src="https://werkidz.mygostore.com/_next/static/images/goStore-logo-64f7ac0ca37c2341f8ae739a75345580.svg"
-            alt="app logo"
-        />
+        <div className={styles.container}>
+            <img
+                className={styles.logo}
+                src="https://werkidz.mygostore.com/_next/static/images/goStore-logo-64f7ac0ca37c2341f8ae739a75345580.svg"
+                alt="app logo"
+            />
 
-        <div className={styles.icons}>
-            <AiOutlineSearch size={24}/>
-            <AiOutlineShopping size={24}/>
+            <div className={styles.icons}>
+                <AiOutlineSearch size={24}/>
+                <AiOutlineShopping size={24}/>
+            </div>
         </div>
     </header>
   )
