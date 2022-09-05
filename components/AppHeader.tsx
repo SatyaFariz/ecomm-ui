c84176668e45acc51c9c4e64431fdf691f6729b7
Satya Fariz <satyafariznur@gmail.com>
2022-09-06 05:38:48 +0700

    check mounted

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from 'next'
 import { AiOutlineSearch, AiOutlineShopping, AiOutlineLeft } from 'react-icons/ai'
-import { useState, useRef, RefObject, ChangeEvent } from 'react'
+import { useState, useRef, RefObject, ChangeEvent, useEffect, MutableRefObject } from 'react'
 import styles from './AppHeader.module.css'
 import { useRouter } from 'next/router'
+import { useDebounce } from 'use-debounce'
 
 const AppHeader: NextPage = () => {
     const [isSearching, setIsSearching] = useState(false)
+    const [searchTerm, setSearchTerm] = useState('test')
     const inputRef: RefObject<HTMLInputElement> = useRef(null)
+    const isMounted: MutableRefObject<boolean> = useRef(false)
     const router = useRouter()
+    const [debouncedSearchTerm] = useDebounce(searchTerm, 500)
 
     const toggleSearch = (isSearching: boolean) => {
         setIsSearching(isSearching)
@@ -16,9 +20,23 @@ const AppHeader: NextPage = () => {
     }
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        router.query.search_term = e.target.value
-        router.replace(router)
+        setSearchTerm(e.target.value)
     }
+
+    useEffect(() => {
+        if(isMounted.current && debouncedSearchTerm.length > 0) {
+            console.log(debouncedSearchTerm)
+            // router.query.search_term = debouncedSearchTerm
+            // router.replace(router)
+        }
+    }, [debouncedSearchTerm])
+
+    useEffect(() => {
+        isMounted.current = true
+        return () => {
+            isMounted.current = false
+        }
+    }, [])
 
     return (
         <header className={styles.header}>
