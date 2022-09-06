1dedff91536389c326665ce4570e29853421447d
Satya Fariz <satyafariznur@gmail.com>
2022-09-06 23:34:42 +0700

    wrap in Hydrate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider, Hydrate } from 'react-query'
 
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <Hydrate state={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+        </Hydrate>
     </QueryClientProvider>
   )
 }
