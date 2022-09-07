c0d99966c9fb14432b379abdf13ba839d6154635
Satya Fariz <satyafariznur@gmail.com>
2022-09-08 04:00:34 +0700

    add customers/me endpoint

---
+++ pages/api/customers/me.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
+import Oauth1Helper from '../../../helpers/Oauth1Helper'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    if(req.method === 'GET') return await post(req, res)
+}
+
+async function post(req: NextApiRequest, res: NextApiResponse) {
+
+    const url = `http://localhost/rest/default/V1/customers/me`
+
+    const request = {
+        url,
+        method: 'GET',
+        headers: req.headers
+    }
+
+    const authHeader = Oauth1Helper.getAuthHeaderForRequest(request)
+    
+    
+    const result = await axios.get(request.url, { headers: { ...authHeader, ...req.headers } })
+    const { data } = result
+
+    data.fullname = data.firstname
+    delete data.firstname
+    delete data.lastname
+
+    res.status(200).json(data)
+}
