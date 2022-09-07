1e22261f1f226137267c5e4a672c9346a0c47d3a
Satya Fariz <satyafariznur@gmail.com>
2022-09-07 17:06:59 +0700

    create sign up file

---
+++ pages/sign-up/index.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import styles from './SignUp.module.css'
+
+const SignUp = (props: any) => {
+    return (
+        <div>
+            SignUp
+        </div>
+    )
+  }
+  
+  export default SignUp
