bad35ee6d20706d9de8feba51f2e3484c66e7caf
Satya Fariz <satyafariznur@gmail.com>
2022-09-16 00:57:17 +0700

    add setHeader and removeHeader

--- libs/http.ts
+++ libs/http.ts
@@ -1,16 +1,24 @@
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
 
 class Http {
-    static instance = axios.create()
+    static instance: AxiosInstance = axios.create()
 
-    static async get(url: string) {
+    static async get(url: string): Promise<any> {
         const res = await this.instance.get(url)
         return res.data
     }
 
-    static async post(url: string, data: any) {
+    static async post(url: string, data: any): Promise<any> {
         const res = await this.instance.post(url, data)
         return res.data
+    }
+
+    static setHeader(key: string, value: string): void {
+        this.instance.defaults.headers.common[key] = value
+    }
+
+    static removeHeader(key: string): void {
+        delete this.instance.defaults.headers.common[key]
     }
 }
 
