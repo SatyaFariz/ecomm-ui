394c226ed278c57b2553eabb6328ce7561589263
Satya Fariz <satyafariznur@gmail.com>
2022-09-19 03:09:26 +0700

    add endpoint to add product to guest-cart

---
+++ pages/api/guest-carts/[cart_id]/items.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
+import Oauth1Helper from '../../../../helpers/Oauth1Helper'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    if(req.method === 'POST') return await post(req, res)
+}
+
+async function post(req: NextApiRequest, res: NextApiResponse) {
+
+    const url = `http://localhost/rest/default/V1/guest-carts/${req.query.cart_id}/items`
+
+    const body = {
+        cartItem: {
+            sku: req.body.sku,
+            quote_id: req.body.quote_id,
+            qty: req.body.qty
+        }
+    }
+
+    const request = {
+        url,
+        method: 'POST',
+        body
+    }
+
+    const authHeader = Oauth1Helper.getAuthHeaderForRequest(request)
+    
+    
+    try {
+        const result = await axios.post(request.url, body, { headers: authHeader })
+        const { data } = result
+        res.status(200).json(data)
+    } catch(error) {
+        console.log(error)
+        const { status } = error.response
+        const payload = {
+            error: true,
+            message: error.response.data.message
+        }
+        res.status(status).json(payload)
+    }
+}
