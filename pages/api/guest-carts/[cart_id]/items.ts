84fc9d3deda44b31b5efbb2e5a67dd38869a424e
Satya Fariz <satyafariznur@gmail.com>
2022-09-19 03:15:47 +0700

    add endpoint to get guest-cart

--- pages/api/guest-carts/[cart_id]/items.ts
+++ pages/api/guest-carts/[cart_id]/items.ts
@@ -7,6 +7,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
     if(req.method === 'POST') return await post(req, res)
+    else if(req.method === 'GET') return await get(req, res)
 }
 
 async function post(req: NextApiRequest, res: NextApiResponse) {
@@ -32,6 +33,34 @@ async function post(req: NextApiRequest, res: NextApiResponse) {
     
     try {
         const result = await axios.post(request.url, body, { headers: authHeader })
+        const { data } = result
+        res.status(200).json(data)
+    } catch(error) {
+        console.log(error)
+        const { status } = error.response
+        const payload = {
+            error: true,
+            message: error.response.data.message
+        }
+        res.status(status).json(payload)
+    }
+}
+
+async function get(req: NextApiRequest, res: NextApiResponse) {
+
+    const url = `http://localhost/rest/default/V1/guest-carts/${req.query.cart_id}/items`
+
+
+    const request = {
+        url,
+        method: 'GET'
+    }
+
+    const authHeader = Oauth1Helper.getAuthHeaderForRequest(request)
+    
+    
+    try {
+        const result = await axios.get(request.url, { headers: authHeader })
         const { data } = result
         res.status(200).json(data)
     } catch(error) {
