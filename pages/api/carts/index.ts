6889b92856b1940bbeb7ed1011417e4a16048dc1
Satya Fariz <satyafariznur@gmail.com>
2022-09-21 04:37:03 +0700

    refactor

--- pages/api/carts/index.ts
+++ pages/api/carts/index.ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
-import Oauth1Helper from '../../../helpers/Oauth1Helper'
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,18 +12,13 @@ async function post(req: NextApiRequest, res: NextApiResponse) {
 
     const url = `http://localhost/rest/default/V1/carts/mine`
 
-    const request = {
-        url,
-        method: 'POST',
-        body: req.body
+    const headers = {
+        authorization: req.headers.authorization as string
     }
-
-    const authHeader = Oauth1Helper.getAuthHeaderForRequest(request)
-    
     
     
     try {
-        const result = await axios.post(request.url, req.body, { headers: { ...authHeader, ...req.headers } })
+        const result = await axios.post(url, req.body, { headers })
         const { data } = result
         res.status(200).json(data)
     } catch(error) {
