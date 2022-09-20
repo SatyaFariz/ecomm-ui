e96591d6ddcbe46580df21ae6265a7877ad8d91d
Satya Fariz <satyafariznur@gmail.com>
2022-09-21 05:14:49 +0700

    add layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider, Hydrate } from 'react-query'
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
 
 const queryClient = new QueryClient()
 
-function MyApp({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+    getLayout?: (page: ReactElement) => ReactNode
+  }
+  
+  type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout
+  }
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+    const getLayout = Component.getLayout ?? ((page) => page)
   return (
     <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
-            <Component {...pageProps} />
+            {getLayout(<Component {...pageProps} />)}
         </Hydrate>
     </QueryClientProvider>
   )
