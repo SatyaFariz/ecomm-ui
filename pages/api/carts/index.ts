2828c2ac5ea35c1a60b481c1b21459c93f89db35
Satya Fariz <satyafariznur@gmail.com>
2022-09-21 03:56:22 +0700

    add create cart endpoint

---
+++ pages/api/carts/index.ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
+import Oauth1Helper from '../../../helpers/Oauth1Helper'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    if(req.method === 'POST') return await post(req, res)
+}
+
+async function post(req: NextApiRequest, res: NextApiResponse) {
+
+    const url = `http://localhost/rest/default/V1/carts/mine`
+
+    const request = {
+        url,
+        method: 'POST',
+        body: req.body
+    }
+
+    const authHeader = Oauth1Helper.getAuthHeaderForRequest(request)
+    
+    
+    
+    try {
+        const result = await axios.post(request.url, req.body, { headers: { ...authHeader, ...req.headers } })
+        const { data } = result
+        res.status(200).json(data)
+    } catch(error) {
+        const { status } = error.response
+        res.status(status).json(null)
+    }
+}
