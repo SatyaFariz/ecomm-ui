aa3228e9a565cc2b32708a5f098930c95305977f
Satya Fariz <satyafariznur@gmail.com>
2022-09-22 05:12:42 +0700

    fix

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -33,8 +33,8 @@ function Cart(props: AppProps) {
         <>
             {cartItems?
             <div className={styles.container}>
-                {cartItems.map((item: any, i: number) =>
-                    <CartItem item={item} key={i}/>
+                {cartItems.map((item: any) =>
+                    <CartItem item={item} key={item.item_id}/>
                 )}
             </div>
             :
