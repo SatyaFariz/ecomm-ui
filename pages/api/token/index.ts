6c50256ff372ece888a3372797bd42f847331569
Satya Fariz <satyafariznur@gmail.com>
2022-09-22 00:22:35 +0700

    refactor

--- pages/api/token/index.ts
+++ pages/api/token/index.ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
-import Oauth1Helper from '../../../helpers/Oauth1Helper'
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,29 +12,15 @@ async function post(req: NextApiRequest, res: NextApiResponse) {
 
     const url = `http://localhost/rest/default/V1/integration/customer/token`
 
-    const request = {
-        url,
-        method: 'POST',
-        body: req.body
-    }
-
-    const authHeader = Oauth1Helper.getAuthHeaderForRequest(request)
-    
-    
-    
     try {
-        const result = await axios.post(request.url, req.body, { headers: authHeader })
+        const result = await axios.post(url, req.body)
         const { data } = result
-        const payload = {
-            token: data
-        }
-        res.status(200).json(payload)
+        res.status(200).json(data)
     } catch(error) {
         const { status } = error.response
         const payload = {
-            error: true,
             message: error.response.data.message
         }
-        res.status(status === 401 ? 200 : status).json(payload)
+        res.status(status).json(payload)
     }
 }
