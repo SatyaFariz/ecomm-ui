1441cef70a267d9e2e00844da4898b2c65f0c1ca
Satya Fariz <satyafariznur@gmail.com>
2022-09-25 21:17:02 +0700

    add loading prop

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,19 @@
 import styles from './Button.module.css'
 import Button from '@mui/material/Button'
+import ReactLoading from 'react-loading'
 
 const CustomButton = (props: any) => {
+    const iconSize = 24
     return (
         <Button 
             variant="contained"
             className={[styles.button, props.className]}
             {...props}
-        />
+        >
+            {props.loading ? 
+            <ReactLoading type="spokes" height={iconSize} width={iconSize}/>
+            : props.children}
+        </Button>
     )
 }
   
