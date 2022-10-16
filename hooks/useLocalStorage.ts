a5ca20d4893e0519f94f6d402c846825c13fb2eb
Satya Fariz <satyafariznur@gmail.com>
2022-10-17 00:14:26 +0700

    fix

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,13 +1,13 @@
 import { useState, useEffect, SetStateAction } from "react";
 
-const useLocalStorage = (key: string): [(string | null)] => {
+const useLocalStorage = (key: string): [(string | null), Function] => {
     const [value, setValue] = useState(null)
 
     useEffect(() => {
         const value = window.localStorage.getItem(key)
         setValue(value as SetStateAction<null>)
     }, [])
-    return [value]
+    return [value, setValue]
 }
 
 export default useLocalStorage;
