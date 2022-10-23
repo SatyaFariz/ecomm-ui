f8a5e02b8c355be1ec183c519759a375dc6b13c0
Satya Fariz <satyafariznur@gmail.com>
2022-10-24 04:19:26 +0700

    add banners mockup

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,13 @@ import { dehydrate, QueryClient } from 'react-query'
 import Link from 'next/link'
 import Head from 'next/head'
 import Http from '../libs/http'
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import { useRouter } from 'next/router'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import Image from 'next/image'
+import 'swiper/css'
+
+import banners from '../mock-data/banners.json'
 
 const graphql = `query search($search_term: String) {
 	products(
@@ -110,6 +115,7 @@ export async function getServerSideProps(context: any) {
 
 const Home = (props: any) => {
     const { dehydratedState } = props
+    const [swiperIndex, setSwiperIndex] = useState(1)
     const router = useRouter()
     const [key, variables] = getKeyAndVariablesFromQuery(router.query)
 
@@ -133,44 +139,72 @@ const Home = (props: any) => {
         getDataFromDehydratedState(cmsPageQueryKey, dehydratedState)
     )
 
+    const handleSwipe = (obj: any) => {
+        setSwiperIndex(obj.activeIndex + 1)
+    }
+
     const { error: userResponseError, data: userResponseData }: any = useAuthedQuery('me', () =>
         Http.get('/api/customers/me')
     )
     
     if (error) return 'An error has occurred: ' + error.message
     return (
-        <div className={styles.container}>
-            {cmsData &&
-                <Head>
-                    <title>{cmsData.data?.cmsPage?.title}</title>
-                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                    <meta name="description" content={cmsData.data?.cmsPage?.meta_description}/>
-                    <meta name="keywords" content={cmsData.data?.cmsPage?.meta_keywords}/>
-                </Head>
-            }
-            <div className="text-blue-500">
-                <Link href='/sign-up'>
-                    Sign Up
-                </Link>
+        <>
+            <div className={styles.bannersContainer}>
+                <Swiper 
+                    onSlideChange={handleSwipe}
+                    virtual={false}
+                    className={styles.image}
+                >
+                    {banners.map((item: any, i: number) => {
+                        return (
+                        <SwiperSlide
+                            key={i}
+                        >
+                            <img
+                                className={styles.image} 
+                                src={item.image}
+                                // layout="fill"
+                                // priority={true}
+                            />
+                        </SwiperSlide>
+                        )
+                    })}
+                </Swiper>
             </div>
-            <div className="text-blue-500">
-                <Link href='/sign-in'>
-                    Sign In
-                </Link>
+            <div className={styles.container}>
+                {cmsData &&
+                    <Head>
+                        <title>{cmsData.data?.cmsPage?.title}</title>
+                        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                        <meta name="description" content={cmsData.data?.cmsPage?.meta_description}/>
+                        <meta name="keywords" content={cmsData.data?.cmsPage?.meta_keywords}/>
+                    </Head>
+                }
+                {/*<div className="text-blue-500">
+                    <Link href='/sign-up'>
+                        Sign Up
+                    </Link>
+                </div>
+                <div className="text-blue-500">
+                    <Link href='/sign-in'>
+                        Sign In
+                    </Link>
+            </div>*/}
+                {(data) ?
+                <>
+                    <ProductList products={data.data?.products?.items}/>
+                    {/* <Pagination 
+                        pageSize={data.search_criteria?.page_size} 
+                        currentPage={data.search_criteria?.current_page}
+                        totalCount={data.total_count} 
+                    /> */}
+                </>
+                :
+                <ProductListShimmer/>
+                }
             </div>
-            {(data) ?
-            <>
-                <ProductList products={data.data?.products?.items}/>
-                {/* <Pagination 
-                    pageSize={data.search_criteria?.page_size} 
-                    currentPage={data.search_criteria?.current_page}
-                    totalCount={data.total_count} 
-                /> */}
-            </>
-            :
-            <ProductListShimmer/>
-            }
-        </div>
+        </>
     )
 }
 
