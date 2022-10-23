a6f89ed81bd412d15cbed0c4c049bda80d19a47b
Satya Fariz <satyafariznur@gmail.com>
2022-10-24 04:47:28 +0700

    fix link

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,12 +1,13 @@
 import NextLink from 'next/link'
 import { Link as MuiLink } from '@mui/material'
 import { forwardRef } from 'react'
+import styles from './Link.module.css'
 
 const Link = forwardRef((props: any, ref: any)=>{
     const { href } = props;
     return (
         <NextLink href={href} passHref >
-            <MuiLink ref={ref} color="inherit" underline="none" {...props}/>
+            <MuiLink ref={ref} className={styles.link} color="inherit" underline="none" {...props}/>
         </NextLink>
     )
 })
