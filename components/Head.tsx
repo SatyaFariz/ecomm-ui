644c34eebed11a6f44a649f8e64756f6d4deb019
Satya Fariz <satyafariznur@gmail.com>
2022-11-25 00:43:37 +0700

    disable browser zoom

--- components/Head.tsx
+++ components/Head.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 const HtmlHead = (props: any) => {
   return (
     <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         {props.children}
     </Head>
   )
