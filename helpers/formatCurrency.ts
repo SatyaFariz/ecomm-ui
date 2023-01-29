ad46a953a33b0dcb4a2bce26365491c65d17e630
Satya Fariz <satyafariznur@gmail.com>
2023-01-29 11:32:55 +0700

    fix format currency

--- helpers/formatCurrency.ts
+++ helpers/formatCurrency.ts
@@ -1,5 +1,14 @@
 const formatCurrency = (number: number): string => {
-    return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', maximumFractionDigits: 0 }).format(number)
+  const chars = number.toString().split('')
+  const newChars: string[] = []
+  let counter = 0
+  while(chars.length > 0) {
+    if(counter % 3 === 0 && counter > 0) newChars.unshift('.')
+    newChars.unshift(chars.pop() as string)
+    counter++
+  }
+
+  return `Rp ${newChars.join('')}`
 }
 
 export default formatCurrency
